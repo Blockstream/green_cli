0417ad60e5730672137fe5420b537a0709dbf098
Jeremy Kauffman <jeremy@blockstream.io>
2020-04-16 09:06:56 +0100

    Add get_asset_info

--- green_cli/green_liquid.py
+++ green_cli/green_liquid.py
@@ -1,4 +1,4 @@
-from green_cli.green import main, with_login, print_result
+from green_cli.green import main, green, with_login, print_result, details_json
 import green_cli.green as basecli
 import click
 
@@ -6,6 +6,15 @@ params = {p.name: p for p in basecli.green.params}
 params['network'].type = click.Choice(['liquid', 'localtest-liquid'])
 params['network'].help = None
 params['network'].default = 'localtest-liquid'
+
+@green.command()
+@click.option('--refresh', is_flag=True, default=False, expose_value=False, callback=details_json)
+@with_login
+@print_result
+def getassetinfo(session, details):
+    details['assets'] = True
+    details['icons'] = False
+    return session.refresh_assets(details)
 
 class Asset(click.ParamType):
     name = 'asset'
