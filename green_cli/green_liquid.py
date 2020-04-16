e91da890e1be7fd621d0880fbb270a89bcd9825f
Jeremy Kauffman <jeremy@blockstream.io>
2020-04-16 09:06:56 +0100

    Add getassetinfo
    A wrapper around GA_refresh_assets (which is slightly misnamed because
    it only refreshes if passed the refresh option, but always returns the
    asset json, possibly from a local cache).

--- green_cli/green_liquid.py
+++ green_cli/green_liquid.py
@@ -1,6 +1,6 @@
 """Modifies base green_cli with liquid specific changes.
 """
-from green_cli.green import main, with_login, print_result
+from green_cli.green import main, green, with_login, print_result, details_json
 import green_cli.green as basecli
 import click
 
@@ -9,6 +9,15 @@ params = {p.name: p for p in basecli.green.params}
 params['network'].type = click.Choice(['liquid', 'localtest-liquid'])
 params['network'].help = None
 params['network'].default = 'localtest-liquid'
+
+@green.command()
+@click.option('--refresh', is_flag=True, default=False, expose_value=False, callback=details_json)
+@with_login
+@print_result
+def getassetinfo(session, details):
+    details['assets'] = True
+    details['icons'] = False
+    return session.refresh_assets(details)
 
 if __name__ == "__main__":
     main()
