77d5a392a6fb45ca6c2fd9d0baf3a5bfade94af4
Jeremy Kauffman <jeremy@blockstream.io>
2020-07-22 10:13:58 +0100

    Add asset as parameter for liquid calls
    However, for liquid mainnet disable sending specific amounts of an asset
    via sendtoaddress because the interface is unsafe.

--- green_cli/green_liquid.py
+++ green_cli/green_liquid.py
@@ -1,7 +1,8 @@
 """Modifies base green_cli with liquid specific changes.
 """
-from green_cli.green import main, green, with_login, print_result, details_json
+from green_cli.green import main, green, with_login, print_result, details_json, _get_network
 from green_cli.liquid_authenticator import WallyAuthenticatorLiquid
+from green_cli.param_types import Address, Amount
 import green_cli.green as basecli
 import click
 
@@ -15,6 +16,7 @@ params['network'].default = 'localtest-liquid'
 params = {p.name: p for p in basecli.createsubaccount.params}
 params['type'].type.choices.append('2of2_no_recovery')
 
+# Add getassetinfo command
 @green.command()
 @click.option('--refresh', is_flag=True, default=False, expose_value=False, callback=details_json)
 @click.option('--icons', is_flag=True, default=False, expose_value=False, callback=details_json)
@@ -23,6 +25,38 @@ params['type'].type.choices.append('2of2_no_recovery')
 def getassetinfo(session, details):
     details['assets'] = True
     return session.refresh_assets(details)
+
+class Asset(click.ParamType):
+    name = 'asset'
+
+    def convert(self, value, param, ctx):
+        assert 'asset_tag' not in ctx.params['details']['addressees'][-1]
+        ctx.params['details']['addressees'][-1]['asset_tag'] = value
+        return value
+
+# Add asset parameter to sendtoaddress but also check for unsafe usage
+@green.command()
+@click.argument('address', type=Address(), expose_value=False)
+@click.argument('asset', type=Asset(), expose_value=False)
+@click.argument('amount', type=Amount(precision=8), expose_value=False)
+@click.option('--subaccount', default=0, expose_value=False, callback=details_json)
+@with_login
+@print_result
+def sendtoaddress(session, details):
+    assets = set([a['asset_tag'] for a in details['addressees']])
+    if _get_network()['mainnet'] and 'send_all' not in details and assets != {'btc'}:
+        # Disable sendtoaddress for non btc assets with amounts on mainnet
+        # The interface is considered unsafe due to the ambiguity of the amount field. For btc the
+        # amount passed to sendtoaddress is interpreted as btc (satoshi x10^8), however assets may
+        # have different 'precision' specified and it's not currently clear how best to handle that.
+        # Leave functionality on for testnet/dev environments as it is convenient
+        raise click.ClickException("Unsafe asset amount conversion disabled")
+    return basecli._send_transaction(session, details)
+
+# Insert asset into addressee option for createtransaction
+params = {p.name: p for p in basecli.createtransaction.params}
+params['addressee'].type = click.Tuple((Address(), Asset(), Amount()))
+params['addressee'].nargs = 3
 
 basecli.WallyAuthenticator = WallyAuthenticatorLiquid
 
