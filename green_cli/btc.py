ed2b274a36f2c55580c44d078ab623ea75defb56
Jeremy Kauffman <jeremy@blockstream.io>
2021-02-23 18:53:51 +0000

    Add tx command and manual coin selection

--- green_cli/btc.py
+++ green_cli/btc.py
@@ -1,6 +1,7 @@
 """Entry point for green-cli (btc)."""
 from green_cli.common import main
 import green_cli.twofa
+import green_cli.tx
 
 if __name__ == "__main__":
     main()
---
+++ green_cli/utils.py
@@ -0,0 +1,19 @@
+import click
+import json
+
+import greenaddress as gdk
+
+from green_cli.gdk_resolve import gdk_resolve
+
+def get_transaction(session, txid):
+    # TODO: Iterate all pages
+    # 900 is slightly arbitrary but currently the backend is limited to 30 pages of 30
+    details = {'subaccount': 0, 'first': 0, 'count': 900}
+    transactions = gdk_resolve(gdk.get_transactions(session.session_obj, json.dumps(details)))
+    transactions = transactions['transactions']
+    for transaction in transactions:
+        if transaction['txhash'] == txid:
+            return transaction
+    raise click.ClickException("Previous transaction not found")
+
+
