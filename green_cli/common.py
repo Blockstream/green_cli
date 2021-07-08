577b7aad1e354b966e1c108b028ea1259033ff4d
Jon Griffiths <jon_p_griffiths@yahoo.com>
2021-07-08 15:35:02 +1200

    Add new filter options to getbalance/getunspentoutputs

--- green_cli/common.py
+++ green_cli/common.py
@@ -343,6 +343,9 @@ def getfeeestimates(session):
 @green.command()
 @click.option('--subaccount', default=0, expose_value=False, callback=details_json)
 @click.option('--num-confs', default=0, expose_value=False, callback=details_json)
+@click.option('--all-coins', type=bool, default=False, expose_value=False, callback=details_json)
+@click.option('--expired-at', type=int, expose_value=False, callback=details_json)
+@click.option('--dust-limit', type=int, expose_value=False, callback=details_json)
 @with_login
 @print_result
 @with_gdk_resolve
@@ -355,6 +358,7 @@ def getbalance(session, details):
 @click.option('--num-confs', default=0, expose_value=False, callback=details_json)
 @click.option('--all-coins', type=bool, default=False, expose_value=False, callback=details_json)
 @click.option('--expired-at', type=int, expose_value=False, callback=details_json)
+@click.option('--dust-limit', type=int, expose_value=False, callback=details_json)
 @with_login
 @print_result
 @with_gdk_resolve
