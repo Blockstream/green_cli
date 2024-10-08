9de43cff60c79177d455fee5cae7277c30483c2d
Jon Griffiths <jon_p_griffiths@yahoo.com>
2024-10-09 09:59:40 +1300

    add project metadata for PyPI uploading

--- setup.py
+++ setup.py
@@ -4,6 +4,33 @@ import green_cli
 setup(
     name='green_cli',
     version=green_cli.version,
+    description='Blockstream Green Command Line Interface',
+    url='https://github.com/Blockstream/green_cli',
+    author='Blockstream',
+    author_email='inquiries@blockstream.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords=[
+        'Blockstream',
+        'Green',
+        'Bitcoin',
+        'wallet',
+        'Liquid',
+        'Elements',
+        'BTC'
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/Blockstream/green_cli/README.md',
+        'Source': 'https://github.com/Blockstream/green_cli',
+        'Tracker': 'https://github.com/Blockstream/green_cli/issues',
+    },
     packages=[
         'green_cli',
         'green_cli/authenticators',
