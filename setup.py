0b6888eba6ea715a06700e883aa30907cf831d9e
Jon Griffiths <jon_p_griffiths@yahoo.com>
2025-03-24 21:11:06 +1300

    deps: update gdk to 0.75.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     install_requires=[
         'wallycore>=1.4.0',
-        'green_gdk>=0.74.0',
+        'green_gdk>=0.75.0',
         'click>=8.1.7',
         'click-repl>=0.3.0',
     ],
